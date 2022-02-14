6fbca7d923203e73cb31135753910b3c41bebf3c
JaviLR81 <javierlzrd@hotmail.com>
2022-02-14 08:54:07 -0600

    Se terminan operaciones para Buyer y Seller Controller

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Buyer;
 
 use App\Http\Controllers\Controller;
+use App\Models\Buyer;
 use Illuminate\Http\Request;
 
 class BuyerController extends Controller
@@ -14,29 +15,14 @@ class BuyerController extends Controller
      */
     public function index()
     {
-        //
+        // Obteniendo unicamente los usuarios que tienen transacciones
+        $compradores = Buyer::has('transactions')->get();
+
+        return response()->json(['data' => $compradores ]);
+        // return $this->showAll($compradores);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -47,39 +33,12 @@ class BuyerController extends Controller
     public function show($id)
     {
         //
+        $comprador = Buyer::has('transactions')->findOrFail($id);
+
+        return response()->json(['data' => $comprador ]);
+
+        // return $this->showOne($buyer);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
--- app/Http/Controllers/Seller/SellerController.php
+++ app/Http/Controllers/Seller/SellerController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Seller;
 
 use App\Http\Controllers\Controller;
+use App\Models\Seller;
 use Illuminate\Http\Request;
 
 class SellerController extends Controller
@@ -15,28 +16,19 @@ class SellerController extends Controller
     public function index()
     {
         //
+
+
+        $vendedores = Seller::has('products')->get();
+
+        return response()->json(['data' => $vendedores ]);
+
+        // $vendedores = Seller::has('products')->get();
+        // return $this->showAll($vendedores);
+
+
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
 
     /**
      * Display the specified resource.
@@ -47,39 +39,11 @@ class SellerController extends Controller
     public function show($id)
     {
         //
+        $vendedor = Seller::has('products')->findOrFail($id);
+        return response()->json(['data' => $vendedor ]);
+
+        // return $this->showOne($seller);
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
