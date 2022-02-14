108966f38638286bd539738e53e1cf024d41d15f
JaviLR81 <javierlzrd@hotmail.com>
2022-02-14 11:07:18 -0600

    Se validan todos los tipos posibles de excepciones en formato JSON

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,9 +3,16 @@
 namespace App\Exceptions;
 
 use App\Traits\ApiResponser;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -56,13 +63,72 @@ class Handler extends ExceptionHandler
 
         // Manejando excepciones de modelo no encontrado
         if ($exception instanceof ModelNotFoundException) {
-            $modelo = class_basename($exception->getModel());
+            $modelo = strtolower(class_basename($exception->getModel()));
             return $this->errorResponse("No existe Ninguna Instancia de {$modelo} con el id espeficico", 404);
         }
 
-        // En caso no sea alguna excepción conocida dejamos que Laravel la maneje
-        return parent::render($request, $exception);
+        // Excepciones para usuarios no autenticados
+        if ($exception instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $exception);
+        }
+
+        // Excepciones para usuarios no autorizados
+        if ($exception instanceof AuthorizationException) {
+            return $this->errorResponse('No posee permisos para ejecutar esta acción', 403);
+        }
+
+        // Excepción para URL no encontrada
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse('No se encontró la URL especificada', 404);
+        }
+
+        // Excepción para manejar métodos HTTP no permitidos en una URl
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse('El método especificado en la petición no es válido', 405);
+        }
+
+        // Controlando diferentes tipos de excepciones HTTP
+        if ($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        // Manejando excepciones para recursos que tienen relaciones entre si en la BD
+        if ($exception instanceof QueryException) {
+            $codigo = $exception->errorInfo[1];
+
+            if ($codigo == 1451) {
+                return $this->errorResponse('No se puede eliminar de forma permamente el recurso porque está relacionado con algún otro.', 409);
+            }
+        }
+
+        // if ($exception instanceof TokenMismatchException) {
+        //     return redirect()->back()->withInput($request->input());
+        // }
+
+        // Solamente en caso de debug vamos regresar posibles excepciones del framework
+        if (config('app.debug')) {
+            // En caso no sea alguna excepción conocida dejamos que Laravel la maneje
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Falla inesperada de la API. Intente luego', 500);
     }
+
+
+    // Devolviendo siempre errores de tipo JSON cuando un usuario no este autenticado
+    // Para determinada acción, este método sobrescribe el por defecto
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('No autenticado.',401);
+    }
+
 
 
      /**
