b0b2b2fbc51e37d9d27653cfc87b9717607af6c7
JaviLR81 <javierlzrd@hotmail.com>
2022-02-18 13:13:44 -0600

    Se agregan tiempos de expiración a los tokens

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -27,7 +27,8 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
-
+        Passport::tokensExpireIn(now()->addMinutes(30));
+        Passport::refreshTokensExpireIn(now()->addMinutes(60));
         //
     }
 }
